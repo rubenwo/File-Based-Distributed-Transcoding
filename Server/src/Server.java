0b78438cc3abf9631dc0c0ce6c36097db809562d
Unknown <rwoldhui@avans.nl>
2018-06-25 16:28:13 +0200

    [Test]

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -4,7 +4,7 @@ public class Server {
 
     public static void main(String[] args) {
         new Thread(new ThreadedServer()).start();
-        
+
         /*String osName = System.getProperty("os.name");
 
         if (osName.contains("Windows")) {
