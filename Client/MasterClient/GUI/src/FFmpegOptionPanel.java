6ad9a92c85efd5dac01b4d8f8e46e2ebbd7a4ac3
Unknown <rwoldhui@avans.nl>
2018-07-07 00:04:37 +0200

    [Fixed] typo && [Added] preset switch based on audio codec

--- Client/MasterClient/GUI/src/FFmpegOptionPanel.java
+++ Client/MasterClient/GUI/src/FFmpegOptionPanel.java
@@ -1,4 +1,3 @@
-import sun.tools.jps.Jps;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,10 +8,10 @@ public class FFmpegOptionPanel extends JPanel {
     private int threads_Min = 1;
     private int threads_Max = Runtime.getRuntime().availableProcessors() + 1;
     private String[][] videoPresets = {{"Select a Video Preset", "ultrafast", "superfast", "veryfast", "faster", "fast", "medium", "slow", "slower", "veryslow", "placebo"}, {"Select a Video Preset"}};
-    private String[][] audioPresets = {{"Select Audio Bitrate", "96", "128", "160", "192", "256", "320"}, {"Select Audo Bitrate"}};
+    private String[][] audioBitrate = {{"Select Audio Bitrate", "96", "128", "160", "192", "256", "320"}, {"Select Audo Bitrate"}};
     private String[] videoEncoderLibs = {"Select a Video Encoder", "libx264", "libx265", "nvenc_hevc", "nvenc_h264"};
     private String[] audioEncoderLibs = {"Select an Audio Encoder", "mp3", "aac", "ac3", "eac3"};
-    private String[] audioChannges = {"Select # of Audio channels (Default = input)", "2.1", "5.1", "7.1"};
+    private String[] audioChannels = {"Select # of Audio channels (Default = input)", "2.1", "5.1", "7.1"};
 
     public FFmpegOptionPanel() {
         this.setLayout(new BorderLayout());
@@ -31,10 +30,10 @@ public class FFmpegOptionPanel extends JPanel {
             crfBox.addItem(Integer.toString(crf));
 
         JComboBox<String> audioEncoderLibsBox = new JComboBox<>(audioEncoderLibs);
-        JComboBox<String> audioPresetsBox = new JComboBox<>(audioPresets[0]);
+        JComboBox<String> audioBitrateBox = new JComboBox<>(audioBitrate[0]);
 
         JComboBox<String> encoderThreadsBox = new JComboBox<>();
-        encoderThreadsBox.addItem("Select # of Threads (Default = all)");
+        encoderThreadsBox.addItem("Select # of Threads (Default = ALL)");
         for (int thread = threads_Min; thread < threads_Max; thread++)
             encoderThreadsBox.addItem(Integer.toString(thread));
 
@@ -48,7 +47,7 @@ public class FFmpegOptionPanel extends JPanel {
             } else if (videoEncoderLibsBox.getSelectedItem().equals("nvenc_hevc") || videoEncoderLibsBox.getSelectedItem().equals("nvenc_h264")) {
                 videoPresetsBox.removeAllItems();
                 for (int i = 0; i < videoPresets[1].length; i++)
-                    videoPresetsBox.addItem(videoPresets[0][i]);
+                    videoPresetsBox.addItem(videoPresets[1][i]);
             }
         });
         videoPresetsBox.addActionListener(e -> {
@@ -59,9 +58,18 @@ public class FFmpegOptionPanel extends JPanel {
         });
         audioEncoderLibsBox.addActionListener(e -> {
             System.out.println(audioEncoderLibsBox.getSelectedItem());
+            if (audioEncoderLibsBox.getSelectedItem().equals("mp3")) {
+                audioBitrateBox.removeAllItems();
+                for (int i = 0; i < audioBitrate[0].length; i++)
+                    audioBitrateBox.addItem(audioBitrate[0][i]);
+            } else if (audioEncoderLibsBox.getSelectedItem().equals("ac3") || audioEncoderLibsBox.getSelectedItem().equals("eac3")) {
+                audioBitrateBox.removeAllItems();
+                for (int i = 0; i < audioBitrate[1].length; i++)
+                    audioBitrateBox.addItem(audioBitrate[1][i]);
+            }
         });
-        audioPresetsBox.addActionListener(e -> {
-            System.out.println(audioPresetsBox.getSelectedItem());
+        audioBitrateBox.addActionListener(e -> {
+            System.out.println(audioBitrateBox.getSelectedItem());
         });
         encoderThreadsBox.addActionListener(e -> {
             System.out.println(encoderThreadsBox.getSelectedItem());
@@ -71,7 +79,7 @@ public class FFmpegOptionPanel extends JPanel {
         dropDownPanel.add(videoPresetsBox);
         dropDownPanel.add(crfBox);
         dropDownPanel.add(audioEncoderLibsBox);
-        dropDownPanel.add(audioPresetsBox);
+        dropDownPanel.add(audioBitrateBox);
         dropDownPanel.add(encoderThreadsBox);
         return dropDownPanel;
     }
