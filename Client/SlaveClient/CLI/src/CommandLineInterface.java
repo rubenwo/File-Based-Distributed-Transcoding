7a48d88bfd90be0e9750ba84e090ef61dc36dd03
Unknown <rwoldhui@avans.nl>
2018-07-15 22:30:08 +0200

    [Added] Empty Pre-app Frames

---
+++ Client/SlaveClient/CLI/src/CommandLineInterface.java
@@ -0,0 +1,4 @@
+public class CommandLineInterface {
+    // This will ask for Server IP-Address AND Server Port number
+    // Then start the SlaveClient as a Command Line Application
+}
---
+++ Client/SlaveClient/GUI/src/PreSlaveFrame.java
@@ -0,0 +1,4 @@
+public class PreSlaveFrame {
+    // This will ask for Server IP-address AND Server Port number.
+    // Then start the SlaveClient as a Graphical User Interface
+}
